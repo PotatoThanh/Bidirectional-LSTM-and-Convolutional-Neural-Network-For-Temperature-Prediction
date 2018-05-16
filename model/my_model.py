ff84e6f1d41a9bc53c70f820ecf0325548ca9ebf
Thanh Nguyen-Duc <thanhnguyen.cse@gmail.com>
2018-05-16 13:38:06 +0900

    Update my_model.py

--- model/my_model.py
+++ model/my_model.py
@@ -57,12 +57,12 @@ def BLSTM_model(input_shape):
         conv1 = Conv1D(64, 2, padding='same')(inputs)
         conv1 = AveragePooling1D(2)(conv1)
 
-    with tf.name_scope('LSTM_forward'):
-        net1 = LSTM(100, go_backwards=False)(conv1)
+    with tf.name_scope('BLSTM_forward'):
+        net1 = Bidirectional(LSTM(80, go_backwards=False))(conv1)
         net1 = Dropout(0.3)(net1)
 
-    with tf.name_scope('LSTM_backward'):
-        net2 = LSTM(100, go_backwards=True)(conv1)
+    with tf.name_scope('BLSTM_backward'):
+        net2 = Bidirectional(LSTM(80, go_backwards=True))(conv1)
         net2 = Dropout(0.3)(net2)
 
     with tf.name_scope('Dense_100'):
